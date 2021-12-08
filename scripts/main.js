ffb3d96bda1ca39dfc5cd5e0374f74ff3761e125
Luis Enrique Gaitan Gaytan <luis_unsc117@outlook.es>
2021-12-07 21:07:55 -0600

    update menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,33 @@ const socialSidebar = document.getElementById('socialSidebar');
 socialIcon.addEventListener('click', () => {
     socialSidebar.classList.toggle('move__social');
     socialIcon.classList.toggle('move__social__icon')
-})
+});
+
+const questions = () => {
+    const titleQuestions = [...document.querySelectorAll(".questions__title")];
+
+    titleQuestions.forEach(question => {
+        question.addEventListener("click", () => {
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle("questions__padding--add");
+            question.children[0].classList.toggle("question__arrow--rotate");
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`
+        });
+    });
+};
+
+questions()
 
 const submit = document.querySelector(".form__submit");
 
 submit.addEventListener("click", (e) => {
     e.preventDefault()
-})
+});
